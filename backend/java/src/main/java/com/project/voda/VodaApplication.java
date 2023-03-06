8b3d1d2dee312cd5469ac72760653d16b9be4fb4
baejunghyun36 <bae1004kin@gmail.com>
2023-03-06 13:58:40 +0900

    [Add] init spring project

---
+++ backend/java/src/main/java/com/project/voda/VodaApplication.java
@@ -0,0 +1,13 @@
+package com.project.voda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodaApplication.class, args);
+	}
+
+}
