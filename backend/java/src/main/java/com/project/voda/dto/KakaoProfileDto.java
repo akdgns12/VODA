474e1b86bf3f7e852ed003aade4ad40e6b3e88c4
akdgns12 <akdgns12@naver.com>
2023-03-13 14:35:41 +0900

    [Add] oauth login dto

--- backend/java/src/main/java/com/project/voda/dto/KakaoProfileDto.java
+++ backend/java/src/main/java/com/project/voda/dto/KakaoProfileDto.java
@@ -4,5 +4,15 @@ import lombok.Data;
 
 @Data
 public class KakaoProfileDto {
-    private String email;
+    public KakaoAccount kakao_account;
+
+    @Data
+    public class KakaoAccount {
+        public Boolean has_email;
+        public Boolean email_needs_agreement;
+        public Boolean is_email_valid;
+        public Boolean is_email_verified;
+        public String email;
+    }
 }
+
