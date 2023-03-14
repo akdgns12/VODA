2bfec37d051b6ae8c37d74770dd22fd81368d4d4
akdgns12 <akdgns12@naver.com>
2023-03-14 09:21:57 +0900

    [Modify] 오타 수정

--- backend/java/src/main/java/com/project/voda/controller/UserController.java
+++ backend/java/src/main/java/com/project/voda/controller/UserController.java
@@ -48,7 +48,7 @@ public class UserController {
     }
   }
 
-  @ApiOperation(value = "회원 가입", notes = "닉네임을 입력받고 회웝가입 하는 API")
+  @ApiOperation(value = "회원 가입", notes = "닉네임을 입력받고 회원가입 하는 API")
   @PostMapping("/signup")
   public ResponseEntity<?> join(UserSignUpRequestDto signUpRequestDto){
     log.info("회원가입 Data : ",signUpRequestDto);
