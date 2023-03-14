128ccab86919570654ffba33b7d99187933c7879
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-03-14 13:26:45 +0900

    [Modify] DailyEmotion - remove calendar, add user, day, 주별차트 API 수정

--- backend/java/src/main/java/com/project/voda/repository/DailyEmotionRepository.java
+++ backend/java/src/main/java/com/project/voda/repository/DailyEmotionRepository.java
@@ -3,12 +3,15 @@ package com.project.voda.repository;
 import com.project.voda.domain.Calendar;
 import com.project.voda.domain.DailyEmotion;
 import com.project.voda.domain.Emotion;
+import com.project.voda.domain.User;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DailyEmotionRepository extends JpaRepository<DailyEmotion, Long> {
 
   // 주별 차트
-  Optional<DailyEmotion> findByCalendarAndEmotion(Calendar calendar, Emotion emotion);
+  List<DailyEmotion> findByUserAndDayBetween(User user, LocalDate fromDate, LocalDate toDate);
 
 }
--- backend/java/src/main/java/com/project/voda/service/ChartServiceImpl.java
+++ backend/java/src/main/java/com/project/voda/service/ChartServiceImpl.java
@@ -11,8 +11,12 @@ import com.project.voda.repository.EmotionRepository;
 import com.project.voda.repository.UserRepository;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -26,40 +30,42 @@ public class ChartServiceImpl implements ChartService {
 
   private final DailyEmotionRepository dailyEmotionRepository;
   private final EmotionRepository emotionRepository;
-  private final CalendarRepository calendarRepository;
   private final UserRepository userRepository;
 
 
   // 주별 차트
   @Override
   public List<ChartWeekResponseDto> getChartWeek(Long userSeq, LocalDate toDate) {
+    LocalDate fromDate = toDate.minusDays(6);
+    log.info("fromDate: {}, toDate: {}", fromDate, toDate);
+
+    List<ChartWeekResponseDto> response = new ArrayList<>();
+
     Optional<User> user = userRepository.findById(userSeq);
     if (user.isEmpty()) {
       return new ArrayList<>();
     }
+    List<DailyEmotion> dailyEmotions = dailyEmotionRepository.findByUserAndDayBetween(user.get(), fromDate, toDate);
 
-    List<ChartWeekResponseDto> response = new ArrayList<>();
-
-    for (Emotion emotion : emotionRepository.findAll()) {
+    emotionRepository.findAll().forEach(emotion -> {
       List<Integer> data = new ArrayList<>();
-      LocalDate fromDate = toDate.minusDays(6);
-      log.info("emotion: {}, fromDate: {}, toDate: {}", emotion.getName(), fromDate, toDate);
-      for (LocalDate day = fromDate; day.isBefore(toDate.plusDays(1)); day = day.plusDays(1)) {
-        Optional<Calendar> calendar = calendarRepository.findByUserAndDay(user.get(), day);
-        if (calendar.isEmpty()) {
-          data.add(0);
-        } else {
-          Optional<DailyEmotion> dailyEmotion = dailyEmotionRepository.findByCalendarAndEmotion(calendar.get(), emotion);
-          if (dailyEmotion.isEmpty()) {
-            data.add(0);
-          } else {
-            data.add(dailyEmotion.get().getCnt());
-          }
-        }
-      }
+
+      List<DailyEmotion> week = dailyEmotions.stream()
+          .filter(m -> m.getEmotion().getEmotionIdx() == emotion.getEmotionIdx())
+          .collect(Collectors.toList());
+
+      Stream.iterate(fromDate, day -> day.isBefore(toDate.plusDays(1)), day -> day.plusDays(1))
+          .map(day -> week.stream().filter(m -> m.getDay().isEqual(day)).findFirst())
+          .forEach(dailyEmotion -> {
+            if (dailyEmotion.isEmpty())
+              data.add(0);
+            else
+              data.add(dailyEmotion.get().getCnt());
+          });
+
       log.info("label: {}, data: {}", emotion.getName(), data);
       response.add(ChartWeekResponseDto.builder().label(emotion.getName()).data(data).build());
-    }
+    });
 
     return response;
   }
