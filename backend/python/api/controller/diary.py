bbe3652e4d8f4f11bf1de17c3c3c1288d9c516cb
whgusehd96 <whgusehd96@gmail.com>
2023-03-16 14:56:22 +0900

    [Feat] response with status code

--- backend/python/api/controller/diary.py
+++ backend/python/api/controller/diary.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, File, Form, UploadFile
+from fastapi import APIRouter, File, Form, UploadFile, status
 from datetime import datetime, date 
 from ..config.database import get_db 
 from ..service import calendar_service 
 from ..service import diary_service
 from experiments.emotion_text.model_call import model_call
+
 router = APIRouter()
 AIMODEL = model_call()
-@router.post("/dairy")
+
+@router.post("/dairy", status_code=status.HTTP_201_CREATED)
 def post_diary(
     voice_file:UploadFile = File(...),
     date : date = Form(...),
@@ -17,4 +19,13 @@ def post_diary(
      calendar = calendar_service.check_exist_calendar(db,date,id)
      diary = diary_service.add_diary(db,AIMODEL,calendar,text_content,"asd")
      sentences = diary_service.add_sentence(db,AIMODEL,diary,calendar,text_content)
-     return {"result" : diary.emotion_idx}
+     result = {
+          "result" : {
+            "sentences": sentences, 
+            "calendar_seq":calendar.calendar_seq, 
+            "diary_seq" : diary.diary_seq,
+            "emotion_idx" : diary.emotion_idx
+            }
+        }
+
+     return result
