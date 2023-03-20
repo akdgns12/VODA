4d45c776bb9eb66cc0a57cddc329eb52eb84fac7
whgusehd96 <whgusehd96@gmail.com>
2023-03-20 10:39:18 +0900

    [Feat] return graph format- lables, data

--- backend/python/api/controller/diary.py
+++ backend/python/api/controller/diary.py
@@ -18,14 +18,17 @@ def post_diary(
      db = get_db() 
      calendar = calendar_service.check_exist_calendar(db,date,id)
      diary = diary_service.add_diary(db,AIMODEL,calendar,text_content,"asd")
-     sentences = diary_service.add_sentence(db,AIMODEL,diary,calendar,text_content)
+     sentences,emotions_cnt = diary_service.add_sentence(db,AIMODEL,diary,calendar,text_content)
+     labels = ["슬픔","놀람","화남","중립","행복"]
      result = {
-          "result" : {
-            "sentences": sentences, 
-            "calendar_seq":calendar.calendar_seq, 
-            "diary_seq" : diary.diary_seq,
-            "emotion_idx" : diary.emotion_idx
-            }
+        "sentences": sentences, 
+        "calendar_seq":calendar.calendar_seq, 
+        "diary_seq" : diary.diary_seq,
+        "emotion_idx" : diary.emotion_idx,
+        "emotion_name" : labels[diary.emotion_idx],
+        "voice_url" : diary.voice_url,
+        "labels" : labels,
+        "data" : emotions_cnt
         }
 
      return result
