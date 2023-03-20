b8b41d5e0eab2518db8da3da41f679b0fa3adb5d
akdgns12 <akdgns12@naver.com>
2023-03-20 15:38:25 +0900

    [Feat] record 경로 router에 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,33 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import SignUp from '../components/SignUp.vue'
-import KakaoLogin from '../components/KakaoLogin.vue'
-import Main from '../components/MainView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import SignUp from "../components/SignUp.vue";
+import KakaoLogin from "../components/KakaoLogin.vue";
+import Main from "../components/MainView.vue";
+import Record from "../audio/components/recorder.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path : '/',
-    component : KakaoLogin
+    path: "/",
+    component: KakaoLogin,
   },
   {
-    path : '/signUp',
-    name : 'signUp',
-    component : SignUp
+    path: "/signUp",
+    name: "signUp",
+    component: SignUp,
   },
   {
-    path : '/main',
-    name : 'main',
-    component : Main
-  }
-]
+    path: "/main",
+    name: "main",
+    component: Main,
+  },
+  {
+    path: "/record",
+    name: "record",
+    component: Record,
+  },
+];
 
 // eslint-disable-next-line no-new
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
