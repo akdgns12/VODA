da5ca5e551334d3aa084913b1eb29680723a199d
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-03-22 17:27:20 +0900

    [Modify] ChartView component margin-top: 16px

--- frontend/src/components/ChartView.vue
+++ frontend/src/components/ChartView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <v-app>
     <h1 align="left">감정곡선</h1>
-    <v-container>
+    <v-container fluid class="pa-0">
       <v-card style="width: 100%" height="50">
         <v-tabs v-model="tab" fixed-tabs bg-color="primary">
           <v-tab v-for="item in items" :key="item.tab">
@@ -12,19 +12,23 @@
           <v-tab-item v-for="item in items" :key="item.tab">
             <v-card flat>
               <v-card-text>
-                <component v-bind:is="item.content"></component>
+                <component
+                  v-bind:is="item.content"
+                  style="margin-top: 16px"
+                ></component>
               </v-card-text>
             </v-card>
           </v-tab-item>
         </v-tabs-items>
       </v-card>
     </v-container>
-  </div>
+  </v-app>
 </template>
 
 <script>
 import WeekChart from "./chart/WeekChart.vue";
 import MonthChart from "./chart/MonthChart.vue";
+
 export default {
   components: {
     WeekChart,
@@ -43,5 +47,10 @@ export default {
 <style>
 h1 {
   margin-left: 16px;
+}
+.v-card__subtitle,
+.v-card__text,
+.v-card__title {
+  padding: 0px;
 }
 </style>
