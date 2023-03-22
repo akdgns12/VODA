ab9ab23e313573ed6be0691ac26755ee12f59bcd
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-03-22 17:31:15 +0900

    [Modify] main.js- add icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
-import axios from 'axios'
-import VCalendar from 'v-calendar';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import axios from "axios";
+import VCalendar from "v-calendar";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
-Vue.prototype.$http = axios
-Vue.config.productionTip = false
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
 Vue.use(VCalendar, {
-  componentPrefix: 'vc',
+  componentPrefix: "vc",
 });
 
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  icons: { iconfont: "md" },
+  render: (h) => h(App),
+}).$mount("#app");
