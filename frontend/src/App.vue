17a4ac559307a90e3b04b1dea8a1fd24843c98e3
yhj <hyjuy1127@gmail.com>
2023-03-23 09:22:06 +0900

    [Add] Bottom nav 컴포넌트로 분리

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,63 @@
 <template>
-  <div id="app">  
-    <router-view/>
-    <v-btn @click = "toSignUp">sign Up </v-btn>
-    <v-btn @click = "toMain"> Main </v-btn>
-  </div>
+<v-app>
+    <div id="app">  
+      <router-view/>
+
+      <BottomNavigation></BottomNavigation>
+
+      <v-btn @click = "toSignUp">sign Up </v-btn>
+      <v-btn @click = "toMain"> Main </v-btn>
+
+
+    </div>
+</v-app>
 </template>
 
 <script>
-
+import {
+  mdiRadioboxMarked,
+  mdiChartBellCurve,
+  mdiCalendarMonth,
+} from "@mdi/js";
+import BottomNavigation from './components/BottomNavigation.vue';
 
 export default {
   name: 'App',
+  data:() => ({
+    value: 3,
+    icons: {
+      mdiChartBellCurve,
+      mdiCalendarMonth,
+      mdiRadioboxMarked,
+    },
+
+  }),
+  components:{
+    BottomNavigation
+  },
   methods:{
     toSignUp(){
-      this.$router.push("/signUp")
+      this.$router.push("/user/signup")
     },
     toMain(){
       this.$router.push("/main")
     }
   },
+  computed: {
+    color() {
+      switch (this.value) {
+        case 0:
+          return "#5AC165";
+        case 1:
+          return "#855CF8";
+        case 2:
+          return "#FF9500";
+        default:
+          return "#5AC165";
+      }
+    },
+  },
+
 }
 </script>
 
@@ -49,7 +88,11 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
     font-weight: 900;
+  }
+  .bottom-nav {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
   }
 </style>
