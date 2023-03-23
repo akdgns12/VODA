f5b66554b043d46ed1866ba56969644190f0fa77
yhj <hyjuy1127@gmail.com>
2023-03-23 09:17:41 +0900

    [Design] 캘린더 디자인 수정

--- frontend/src/components/MainView.vue
+++ frontend/src/components/MainView.vue
@@ -1,24 +1,4 @@
-<template>
-  <v-app>
-    <v-container fluid pa-0>   
-      <!-- <v-layout class="d-flex justify-center align-center" >
-          <v-col cols="auto">
-            <v-btn icon @click="prev">
-              <v-icon dark>mdi-chevron-left</v-icon>  
-            </v-btn>
-          </v-col>
-          <v-col cols="auto"> 
-            <div class="my-header">
-              <span>{{ focus.getFullYear() }}년 </span>
-              <span>{{ focus.getMonth() + 1 }}월</span>
-            </div>
-          </v-col >
-          <v-col cols="auto">
-            <v-btn icon @click="next">
-              <v-icon dark>mdi-chevron-right</v-icon>
-            </v-btn>
-          </v-col>
-      </v-layout> -->
+<template> 
       <v-layout>
         <v-flex xs12 class="mb-3">
           <v-sheet height="500">
@@ -39,47 +19,9 @@
           </v-sheet>
         </v-flex>
       </v-layout>
-
-      <v-layout class="bottom-nav">
-        <v-bottom-navigation
-          v-model="value"
-          :background-color="color"
-          dark
-          class="d-flex justify-between"
-          grow
-        >
-          <v-btn>
-            <span></span>
-            <v-icon large>
-              {{ icons.mdiCalendarMonth }}
-            </v-icon>
-          </v-btn>
-
-          <v-btn>
-            <v-icon color="red" size="60">
-              {{ icons.mdiRadioboxMarked }}
-            </v-icon>
-          </v-btn>
-
-          <v-btn>
-            <span></span>
-            <v-icon large>
-              {{ icons.mdiChartBellCurve }}
-            </v-icon>
-          </v-btn>
-        </v-bottom-navigation>
-      </v-layout>
-    </v-container>
-  </v-app>
 </template>
 
 <script>
-import {
-  mdiRadioboxMarked,
-  mdiChartBellCurve,
-  mdiCalendarMonth,
-} from "@mdi/js";
-
 export default {
   data: () => ({
     type: "month",
@@ -94,12 +36,6 @@ export default {
       { text: "Custom Daily", value: "custom-daily" },
       { text: "Custom Weekly", value: "custom-weekly" },
     ],
-    icons: {
-      mdiChartBellCurve,
-      mdiCalendarMonth,
-      mdiRadioboxMarked,
-    },
-    value: 3,
     masks: {
       weekdays: "WWW",
     },
@@ -114,22 +50,11 @@ export default {
       ],
     mediaStream: null,
   }),
+  created(){
+    this.$store.dispatch("setShowBottomNavigation", true);
+  },
   mounted() {
     // this.$refs.calendar.checkChange();
-  },
-  computed: {
-    color() {
-      switch (this.value) {
-        case 0:
-          return "#5AC165";
-        case 1:
-          return "#855CF8";
-        case 2:
-          return "#FF9500";
-        default:
-          return "#5AC165";
-      }
-    },
   },
   methods: {
     prev() {
@@ -157,64 +82,20 @@ export default {
           ${day.day}
       `;
     },
-    startRecording(){
-      const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  
-      if (isMobile) {
-        // 모바일 장비인 경우
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-          navigator.mediaDevices.getUserMedia({ audio: true })
-            .then((stream) => {
-              this.stream = stream;
-              // 마이크 접근 권한이 허용된 경우
-            })
-            .catch((error) => {
-              // 마이크 접근 권한이 거부된 경우
-              if (error.name === 'NotAllowedError') {
-                alert('마이크 접근 권한이 필요합니다.');
-              } else if (error.name === 'NotFoundError' || error.name === 'NotReadableError') {
-                alert('마이크를 찾을 수 없습니다.');
-              } else {
-                alert('마이크에 접근할 수 없습니다.');
-              }
-            });
-        } else {
-          alert('미디어 입력 장치를 지원하지 않습니다.');
-        }
-      } 
-      else {
-        // 데스크톱 장비인 경우
-        navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-        if (navigator.getUserMedia) {
-          navigator.getUserMedia({ audio: true },
-            (stream) => {
-              this.stream = stream;
-              // 마이크 접근 권한이 허용된 경우
-            },
-            () => {
-              // 마이크 접근 권한이 거부된 경우
-              alert('마이크 접근 권한이 필요합니다.');
-            });
-        } else {
-          alert('미디어 입력 장치를 지원하지 않습니다.');
-        }
-      }
-    }
   }
 };
 </script>
 
 <style scoped lang="scss">
+@font-face {
+  font-family: 'ComingSoon-Regular';
+  src: url('../../public/fonts/ComingSoon-Regular.ttf');
+}
 .my-header {
   align-items: center;
   justify-content: center;
   font-size: 24px;
   padding: 5px 0;
-}
-.bottom-nav {
-  position: fixed;
-  bottom: 0;
-  width: 100%;
 }
 
 @media only screen and (max-width: 600px) {
@@ -231,12 +112,11 @@ export default {
 .day2 {
   margin: 10px;
 }
-.vc-title {
-  color: "red";
-}
+
 .day-header {
   text-align: center;
-  font-size: 15px;
+  font-size: 18px;
+  font-family: 'ComingSoon-Regular';
 }
 ::-webkit-scrollbar-track {
   display: none;
@@ -244,6 +124,7 @@ export default {
 //달력 테두리
 ::v-deep .custom-calendar.vc-container {
   //달력 내부 선
+  margin-top:30px;
   --day-border: 1px solid #b8c2cc;
   --day-border-highlight: 1px solid #b8c2cc;
   //날짜별 크기
@@ -253,15 +134,24 @@ export default {
   --weekday-bg: #f8fafc;
   //요일 테두리 색상
   --weekday-border: 1px solid #eaeaea;
-
+  .vc-title {
+    color: beige;
+    font-family: 'SigmarOne-Regular';
+    font-family: 'ComingSoon-Regular';
+    font-size: 25px;
+  }
     border-radius: 10px 10px 0px 0px;
     //달력 너비
     width: 90%;
-
+  .vc-arrow{
+    color:beige;
+    background-color: red;
+  }
   .vc-header {
     padding: 10px 0;
     background-color: red;
     border-radius: 10px 10px 0px 0px;
+
   }
   .vc-weeks {
     padding: 0;
@@ -272,6 +162,7 @@ export default {
     border-top: var(--weekday-border);
     padding: 5px 0;
     color: black;
+    font-family: 'ComingSoon-Regular';
     
   }
   .vc-day {
