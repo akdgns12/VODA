b434d81d6bb3306ddc317b38d2681378e0ba16cd
whgusehd96 <whgusehd96@gmail.com>
2023-03-24 10:29:01 +0900

    [Feat] remove diary with related entity

--- backend/java/src/main/java/com/project/voda/controller/DiaryController.java
+++ backend/java/src/main/java/com/project/voda/controller/DiaryController.java
@@ -1,17 +1,44 @@
 package com.project.voda.controller;
 
 import com.project.voda.service.DiaryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/diary")
 @Slf4j
+@Api(tags = "다이어리 API - 수정,삭제")
 public class DiaryController {
 
   private final DiaryService diaryService;
 
+  @ApiImplicitParam(name = "diarySeq", value = "diary PK")
+  @DeleteMapping("/diary/{diarySeq}")
+  ResponseEntity<?> removeDiary(@PathVariable Long diarySeq){
+    log.info("delete diary : {}", diarySeq);
+    try{
+      diaryService.removeDiary(diarySeq);
+      return new ResponseEntity<>(HttpStatus.OK);
+    }catch (Exception e){
+      return exceptionHandling(e);
+    }
+  }
+
+  private ResponseEntity<?> exceptionHandling(Exception e) {
+    Map<String, String> map = new HashMap<>();
+    map.put("message", e.getMessage());
+    return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 }
--- backend/java/src/main/java/com/project/voda/service/DiaryServiceImpl.java
+++ backend/java/src/main/java/com/project/voda/service/DiaryServiceImpl.java
@@ -1,16 +1,64 @@
 package com.project.voda.service;
 
+import com.project.voda.domain.*;
+import com.project.voda.repository.CalendarRepository;
+import com.project.voda.repository.DailyEmotionRepository;
+import com.project.voda.repository.DiaryRepository;
+import com.project.voda.repository.SentenceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 @Slf4j
 public class DiaryServiceImpl implements DiaryService{
 
+    private final DiaryRepository diaryRepository;
+    private final DailyEmotionRepository dailyEmotionRepository;
+    private final CalendarRepository calendarRepository;
 
+    @Override
+    public Boolean removeDiary(Long diarySeq) {
+        Optional<Diary> optionalDiary = diaryRepository.findById(diarySeq);
+        if(optionalDiary.isEmpty()){
+            return false;
+        }
+        Diary diary = optionalDiary.get();
+        List<Sentence> sentences = diary.getSentences();
+        Calendar calendar = diary.getCalendar();
+        List<DailyEmotion> dailyEmotions = dailyEmotionRepository.findByUserAndDay(calendar.getUser(),calendar.getDay());
+        int[] emotionSubCnt = new int[5];
+        sentences.stream().forEach(x->emotionSubCnt[x.getEmotion().getEmotionIdx()]++);
 
+        int maxCnt = 0;
+        Emotion maxEmotion = null;
+        for(DailyEmotion dailyEmotion : dailyEmotions){
+            dailyEmotion.subCnt(emotionSubCnt[dailyEmotion.getEmotion().getEmotionIdx()]);
+            if(dailyEmotion.getCnt() >= maxCnt){
+                maxEmotion = dailyEmotion.getEmotion();
+                maxCnt = dailyEmotion.getCnt();
+            }
+        }
+        diaryRepository.delete(diary);
+
+        if(maxCnt == 0 ){
+            for(DailyEmotion dailyEmotion : dailyEmotions){
+                dailyEmotionRepository.delete(dailyEmotion);
+            }
+            calendarRepository.delete(calendar);
+        }else {
+            calendar.updateEmotion(maxEmotion);
+        }
+
+        /**
+         * voice 파일 삭제
+         */
+        return true;
+    }
 }
