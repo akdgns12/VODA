02422a914b0a090f679759d94ac32ce8a753889b
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-03-24 17:07:46 +0900

    [Modify] MonthDatePicker date control, props, emit

--- frontend/src/components/chart/MonthDatePicker.vue
+++ frontend/src/components/chart/MonthDatePicker.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row no-gutters>
     <v-col cols="2">
-      <v-btn height="50px"><</v-btn>
+      <v-btn height="50px" @click="before()"><</v-btn>
     </v-col>
     <v-col cols="8">
       <v-dialog
@@ -14,7 +14,7 @@
       >
         <template v-slot:activator="{ on, attrs }">
           <v-text-field
-            v-model="date"
+            v-model="dateString"
             append-icon="mdi-calendar"
             readonly
             v-bind="attrs"
@@ -22,13 +22,13 @@
           ></v-text-field>
         </template>
         <v-date-picker
-          v-model="date"
+          v-model="dateString"
           type="month"
           :active-picker.sync="activePicker"
           :max="
             new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
               .toISOString()
-              .substr(0, 10)
+              .substr(0, 7)
           "
           min="1950-01-01"
           @change="save"
@@ -36,17 +36,20 @@
       </v-dialog>
     </v-col>
     <v-col cols="2">
-      <v-btn height="50px">></v-btn>
+      <v-btn height="50px" @click="after()">></v-btn>
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
+  props: {
+    date: Date,
+  },
   data: () => ({
     activePicker: null,
-    date: "2023-03",
     menu: false,
+    dateString: null,
   }),
   watch: {
     menu(val) {
@@ -55,8 +58,32 @@ export default {
   },
   methods: {
     save(date) {
+      this.date.setMonth(new Date(date).getMonth());
+      this.date.setFullYear(new Date(date).getFullYear());
+      this.$emit("change", this.date);
       this.$refs.menu.save(date);
     },
+    before() {
+      this.$emit(
+        "change",
+        new Date(this.date.setMonth(this.date.getMonth() - 1))
+      );
+      this.dateString = this.date.toISOString().substring(0, 7);
+    },
+    after() {
+      if (new Date().getMonth() === this.date.getMonth()) {
+        alert("이번달까지만 조회 가능합니다");
+        return;
+      }
+      this.$emit(
+        "change",
+        new Date(this.date.setMonth(this.date.getMonth() + 1))
+      );
+      this.dateString = this.date.toISOString().substring(0, 7);
+    },
+  },
+  created() {
+    this.dateString = this.date.toISOString().substring(0, 7);
   },
 };
 </script>
