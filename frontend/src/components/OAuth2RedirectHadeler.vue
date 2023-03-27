eac43d9e6ad771f643dd67fb83722f5eca770b09
yhj <hyjuy1127@gmail.com>
2023-03-27 14:41:39 +0900

    [Modify] login 통신

--- frontend/src/components/OAuth2RedirectHadeler.vue
+++ frontend/src/components/OAuth2RedirectHadeler.vue
@@ -1,45 +1,37 @@
 <template>
-  <div class = "spinner-div" v-if="isLoading">
-    로딩 중입니다.
+  <div class = "spinner-div">
+    <div v-if="isLoading">
+      로딩 중입니다.
+    </div>    
   </div>   
 </template>
 
 <script>
-import userStore from '@/store/modules/userStore';
+
 export default {
   name: 'RedirectHandler',
     data(){
         return{
-          isLoading:true
+          isLoading:true,
         }
     },
     async mounted(){
       try{
         
-
         let params = new URL(window.location.href).searchParams;
         const code = params.get("code");
-        console.log(code);
-         
-        this.$store.dispatch("getUserInfo", { data : code })
-        console.log(userStore.state.userData)
-      
-        // await api.get("/user/login/oauth/kakao")
-        //   .then((response) => {
-        //         console.log(response)
-        //         if (response.data.errorCode === 400) {
-        //           alert(response.data.errorMessage)
 
-        //           }
-        //         else{
-        //           alert("회원가입이 완료되었습니다. 로그인 화면으로 돌아갑니다")
-        //           this.$router.push({path: '/'});
-        //         }
-        //         })
-        // .catch(error =>{
-        //   console.log(error.response);
+        await this.$store.dispatch("getUserInfo", { data : code });
+        const userData = this.$store.getters.userData;
 
-        // });
+        this.isLoading = false;
+
+        if (this.$store.getters.userStatus == 200) {
+          this.$router.push(`/calendar/${userData.userSeq}`);
+        } else {
+          // 없으면 회원가입 페이지로 라우팅
+          this.$router.push("/user/signup");
+        }
       } catch(error){
         console.log(error)
       }
