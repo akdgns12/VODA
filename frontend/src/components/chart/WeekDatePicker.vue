a1f00e491525bcfc05fe4cbac42d33d663ecdf79
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-03-29 16:42:19 +0900

    [Modify] WeekDatePicker 1 week control
    Former-commit-id: 6db6c176857dcd3ef25f0481c09d20b7f6a5c148

--- frontend/src/components/chart/WeekDatePicker.vue
+++ frontend/src/components/chart/WeekDatePicker.vue
@@ -7,7 +7,7 @@
       <v-dialog
         ref="modal"
         v-model="modal"
-        :close-on-content-click="false"
+        :close-on-content-click="true"
         transition="scale-transition"
         offset-y
         max-width="290px"
@@ -19,11 +19,19 @@
             append-icon="mdi-calendar"
             persistent-hint
             v-bind="attrs"
-            @blur="date = parseDate(dateFormatted)"
             v-on="on"
           ></v-text-field>
         </template>
-        <v-date-picker v-model="dates" range></v-date-picker>
+        <v-date-picker
+          v-model="dates"
+          range
+          :max="
+            new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+              .toISOString()
+              .substr(0, 10)
+          "
+          min="1950-01-01"
+        ></v-date-picker>
       </v-dialog>
     </v-col>
     <v-col cols="2">
@@ -34,14 +42,42 @@
 
 <script>
 export default {
+  props: {
+    startDate: Date,
+    endDate: Date,
+  },
   data: () => ({
-    dates: ["2023-03-16", "2023-03-22"],
+    dates: [],
     modal: false,
   }),
   computed: {
     dateRangeText() {
       return this.dates.join(" ~ ");
     },
+  },
+  watch: {
+    dates(val) {
+      console.log("[watch] dates: ", val);
+      if (val.length == 1) {
+        this.endDate.setDate(new Date(val[0]).getDate());
+        this.endDate.setMonth(new Date(val[0]).getMonth());
+        this.endDate.setFullYear(new Date(val[0]).getFullYear());
+        this.startDate.setDate(new Date(val[0]).getDate() - 6);
+        this.startDate.setMonth(new Date(val[0]).getMonth());
+        this.startDate.setFullYear(new Date(val[0]).getFullYear());
+        this.$emit("change", this.startDate, this.endDate);
+
+        this.dates = [];
+        this.dates.push(this.startDate.toISOString().substr(0, 10));
+        this.dates.push(this.endDate.toISOString().substr(0, 10));
+        this.$refs.modal.save(this.dates);
+      }
+    },
+  },
+  methods: {},
+  created() {
+    this.dates.push(this.startDate.toISOString().substr(0, 10));
+    this.dates.push(this.endDate.toISOString().substr(0, 10));
   },
 };
 </script>
